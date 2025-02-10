03d77a282f59c1c97a4cf71bbdfd0f5957e37b3e
Bilel Jabrane <bileljabrane6@gmail.com>
2025-02-10 11:19:37 +0100

    Loading screen fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import './index.css'
 import { LoadingScreen } from './components/LoadingScreen'
-
-
+import { useState } from 'react'
 function App() {
-
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <>
-      <LoadingScreen></LoadingScreen>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
     </>
   )
 }
